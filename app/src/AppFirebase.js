3d236633f6c153fb91a9dca2f84d04596d4a4c5c
Jakub <jakubfedyk@gmail.com>
2017-12-16 14:02:18 +0100

    firebase

--- app/src/AppFirebase.js
+++ app/src/AppFirebase.js
@@ -1,9 +1,35 @@
-import { Component } from 'react';
-import { database } from './firebase';
+import React, {Component} from 'react';
+import {database} from './firebase';
 
 class AppFirebase extends Component {
+    state = {
+        counter: null
+    };
+    incHandler=()=>{
+        database.ref('/counter')
+    .set(this.state.counter + 1)
+    };
+    incHandler=()=>{
+        database.ref('/counter')
+    .set(this.state.counter - 1)
+    };
+
+    componentDidMount() {
+        database.ref('/counter')
+            .once('value', (snapshot) => {
+                this.setState({
+                    counter: snapshot.val()
+                })
+            })
+    }
+
     render() {
-        return null
+        return <div>
+            <h1>{this.state.counter}</h1>
+            <button onClick = {this.incHandler}></button>
+            <button onClick = {this.incHandler}></button>
+        </div>
     }
 }
+
 export default AppFirebase
