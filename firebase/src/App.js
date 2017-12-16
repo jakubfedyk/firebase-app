b4e4498c741b0a1a3c63f60593645cd5fde1474f
Jakub <jakubfedyk@gmail.com>
2017-12-16 12:04:07 +0100

    adding PUT PATCH DELETE

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -11,10 +11,37 @@ class App extends Component {
                 this.setState({
                     response: JSON.stringify(json)
                 })
-            })
+            }).then(()=> console.log('GET DONE'))
     };
+    putHandler = () => {
+        fetch("https://ad-snadbox.firebaseio.com/users/jakub.json",
+            {
+                method: 'PUT',
+                body: JSON.stringify({
+                    name: 'Jakub',
+                    lastname: 'Fedyk',
+                    active: false
+                })
+            }).then(()=> console.log('PUT DONE'))
+        };
+    patchHandler = () => {
+        fetch("https://ad-snadbox.firebaseio.com/users/jakub.json",
+            {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    active: true
+                })
+            }).then(()=> console.log('PATCH DONE'))
 
-        postHandler = () => {
+    };
+    deleteHandler = () => {
+            fetch("https://ad-snadbox.firebaseio.com/users/jakub.json",
+            {
+                method:'DELETE'
+                }).then(()=> console.log('DELETE DONE'))
+            };
+
+    postHandler = () => {
             fetch("https://ad-snadbox.firebaseio.com/users/jakub.json",
                 {
                     method: 'POST',
@@ -23,13 +50,16 @@ class App extends Component {
                         lastname: 'Fedyk',
                         active: false
                     })
-                })
+                }).then(()=> console.log('POST DONE'))
         };
     render() {
         return (
             <div className="App">
                 <button onClick={this.getHandler}>GET data from our endpoint</button>
                 <button onClick={this.postHandler}>POST data from our endpoint</button>
+                <button onClick={this.putHandler}>PUT data from our endpoint</button>
+                <button onClick={this.patchHandler}>PATCH data from our endpoint</button>
+                <button onClick={this.deleteHandler}>DELETE data from our endpoint</button>
                 <p>
                     {this.state.response}
                 </p>
