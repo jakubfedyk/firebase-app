f4f29b5dd831b4664e9056f58386e713ca0baf45
Jakub <jakubfedyk@gmail.com>
2017-12-16 11:41:37 +0100

    add POST

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -14,10 +14,22 @@ class App extends Component {
             })
     };
 
+        postHandler = () => {
+            fetch("https://ad-snadbox.firebaseio.com/users/jakub.json",
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: 'Jakub',
+                        lastname: 'Fedyk',
+                        active: false
+                    })
+                })
+        };
     render() {
         return (
             <div className="App">
                 <button onClick={this.getHandler}>GET data from our endpoint</button>
+                <button onClick={this.postHandler}>POST data from our endpoint</button>
                 <p>
                     {this.state.response}
                 </p>
