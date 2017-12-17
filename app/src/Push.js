834484240b10b246943aaee8fd68b0db7ba64950
Jakub <jakubfedyk@gmail.com>
2017-12-17 10:49:42 +0100

    push ver 2

--- app/src/Push.js
+++ app/src/Push.js
@@ -3,6 +3,9 @@ import {database} from './firebase';
 
 
 class Push extends Component {
+    state = {
+        pushList: null
+    };
 
     componentDidMount() {
         const newRef = database.ref('pushList').push();
@@ -10,14 +13,33 @@ class Push extends Component {
         newRef.set('Data to set in newRef')
         const newRef2 = database.ref('pushList').push('Data to set directly in push!');
         console.log('newRef2', newRef2)
+        database.ref('pushList')
+            .on('value', (snapshot) => {
+                this.setState({
+                    pushList: snapshot.val()
+                })
+            });
     }
+
     render() {
         return (
             <div>
                 Push
+                <ul>
+                    {
+                        this.state.pushList
+                        &&
+                        Object.entries(this.state.pushList)
+                            .map(([key, value]) => (
+                                <li key={key}>
+                                    {value}
+                                </li>
+                            ))
+                    }
+                </ul>
             </div>
         )
     }
 }
 
-export default Push
+export default Push;
